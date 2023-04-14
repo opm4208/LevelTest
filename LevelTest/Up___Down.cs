adb59c175f87b7bd324707cc287f231a064cfd25
민 박 <opm4208@gmail.com>
2023-04-14 20:44:07 +0900

    feat: UP_DOWN게임
    랜덤으로 숫자를 생성하여 사용자가 숫자를 입력하여 10번의 기회안에 정답을 맞춘다 사용자가 입력한 숫자보다 정답이 크면 UP, 낮으면 DOWN을 출력한다 10번의 기회안에 못 맞췄을 경우 Y버튼을 누르면 다시시작 N버튼을 누르면 게임이 종료된다.

---
+++ LevelTest/Up___Down.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class Up___Down
+    {
+        static public int Randomint()
+        {
+            Random rand = new Random();
+            int num = rand.Next(0, 1000);
+            return num;
+        }
+        static public void UpDownGame()
+        {
+            int randomnum = Randomint();
+            int count = 10;
+            while (count > 0)
+            {
+                Console.WriteLine("숫자를 입력해 주세요.");
+                int number = int.Parse(Console.ReadLine());
+                if (number == randomnum)
+                {
+                    Console.WriteLine("정답입니다.");
+                    break;
+                }
+                else if (number < randomnum)
+                {
+                    Console.WriteLine("Up");
+                    count--;
+                }
+                else if (number > randomnum)
+                {
+                    Console.WriteLine("Down");
+                    count--;
+                }
+            }
+            if (count == 0)
+            {
+                bool check = true;
+                Console.WriteLine("다시 시작하겠습니까?");
+                while (check)
+                {
+                    Console.WriteLine("다시하기 y. 그만하기 n를 입력해 주세요.");
+                    string str = Console.ReadLine();
+                    switch (str)
+                    {
+                        case "y":
+                        case "Y":
+                            Console.WriteLine("게임을 다시 시작하겠습니다.");
+                            check = false;
+                            UpDownGame();
+                            break;
+                        case "n":
+                        case "N":
+                            Console.WriteLine("게임이 종료됩니다.");
+                            check = false;
+                            break;
+                        default:
+                            Console.WriteLine("잘못된 입력입니다 다시 입력해 주세요.");
+                            break;
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            UpDownGame();
+        }
+
+    }
+}
