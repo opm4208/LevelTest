8e72f9d06143d78cb0774688d96c415342997e02
민 박 <opm4208@gmail.com>
2023-04-14 20:29:09 +0900

    feat: 소수인지 판별하기
    매개변수로 int형을 받아서 for문과 if문을 사용하여 소수이면 true 반환 소수가 아니면 false를 반환하게 한다.

---
+++ LevelTest/IsPrime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class IsPrime
+    {
+        static bool IsPrimes(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
