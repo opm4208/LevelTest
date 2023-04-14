ba204478be919b9bf453932f97f3edb5c350e17e
민 박 <opm4208@gmail.com>
2023-04-14 20:39:45 +0900

    feat: 정렬된 배열에서 공통된 숫자를 찾는다
    매개변수로 int[] 3개를 매개변수로 받아 for문을 이용해 값이 같으면 bool[] 을 이용해 둘다 true일때 리시트에 값을 추가하고 나중에 중복을 제거하고 배열로 변환하여 반환한다.

---
+++ LevelTest/FindCommonItems.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class FindCommonItems
+    {
+        static int[] FindCommonItemse(int[] arr1, int[] arr2, int[] arr3)
+        {
+            bool[] checkct = { false, false };
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        checkct[0] = true;
+                        break;
+                    }
+                    else if (arr1[i] < arr2[j])
+                    {
+                        break;
+                    }
+                }
+                for (int k = 0; k < arr3.Length; k++)
+                {
+                    if (arr1[i] == arr3[k])
+                    {
+                        checkct[1] = true;
+                        break;
+                    }
+                    else if (arr1[i] < arr3[k])
+                    {
+                        break;
+                    }
+                }
+                if (checkct[0] && checkct[1])
+                {
+                    list.Add(arr1[i]);
+                }
+                checkct[0] = false;
+                checkct[1] = false;
+            }
+            list = list.Distinct().ToList();
+            int[] ints = list.ToArray();
+            return ints;
+        }
+
+    }
+}
