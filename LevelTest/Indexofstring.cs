3c028423c6f3336996404250ac6f39244727c78d
민 박 <opm4208@gmail.com>
2023-04-14 20:14:15 +0900

    feat: 문자열속의 문자를 찾는다
    문자열을 입력받은 문자열에서 문자열을 다시 입력받아 다시 입력받은 문자열이 있으면 그위치를 반환한다 없을시 -1로 반환

---
+++ LevelTest/Indexofstring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class Indexofstring
+    {
+        static public void Indexofstrings()
+        {
+            Console.WriteLine("문자열을 입력해 주세요.");
+            string str = Console.ReadLine();
+            Console.WriteLine("문자열에서 찾을 문자열을 입력해 주세요.");
+            string s = Console.ReadLine();
+            Console.WriteLine(str.IndexOf(s));
+        }
+
+    }
+}
