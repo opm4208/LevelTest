61a69946446e6ad229e5f6961e167796284e2b30
민 박 <opm4208@gmail.com>
2023-04-14 20:23:36 +0900

    feat: 문자열의 단어의 갯수를 출력한다
    문자열을 입력받아 Split()을 사용해 문자열을 배열에 저장하여 단어의 갯수를 구한다.

---
+++ LevelTest/stringCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class stringCount
+    {
+        static public void stringCounts()
+        {
+            Console.WriteLine("문자열을 입력해 주세요.");
+            string s = Console.ReadLine();
+            string[] str = s.Split(' ');
+            Console.WriteLine(str.Length);
+        }
+
+    }
+}
