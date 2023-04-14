bb26434a2ae6aedb713de1b29ff767a05dc2aa0e
민 박 <opm4208@gmail.com>
2023-04-14 20:32:40 +0900

    feat: 양의 정수의 각 자리수의 합을 구한다
    매개변수로 int형 자료를 받아 string으로 변환 후 각 자릿수로 나누기 위해 char[] 배열로 변환 후 for문으로 각자리수를 더한 값을 반환한다.

---
+++ LevelTest/SumOfDigitscs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest
+{
+    internal class SumOfDigitscs
+    {
+        static int SumOfDigitse(int num)
+        {
+            string str = string.Format("{0}", num);
+            char[] strarray = str.ToCharArray();
+            int[] nums = new int[strarray.Length];
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(strarray[i].ToString());
+                Console.WriteLine(strarray[i]);
+                sum += nums[i];
+            }
+            return sum;
+        }
+
+    }
+}
